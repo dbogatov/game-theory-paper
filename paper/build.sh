4545691c0446dade16f37fc706d9a9e80e45d8fa
Dmytro <dbogatov@wpi.edu>
2017-02-03 17:04:57 -0500

    Add CI.

--- paper/build.sh
+++ paper/build.sh
@@ -3,7 +3,7 @@
 INTERACTION=nonstopmode
 OUTDIR=dist
 COMPILATIONS=3
-JOBNAME=presenation
+JOBNAME=paper
 
 echo "Generating document..."
 
@@ -19,7 +19,7 @@ echo "Attaching appendicies..."
 
 enscript -o - src/appendicies/APPENDIX_A.swift | ps2pdf - > dist/APPENDIX_A.pdf
 enscript -o - src/appendicies/APPENDIX_B.txt | ps2pdf - > dist/APPENDIX_B.pdf
-pdfjoin dist/paper.pdf dist/APPENDIX_A.pdf dist/APPENDIX_B.pdf --outfile dist/paper.pdf --rotateoversize false &>/dev/null
+pdfjoin dist/${JOBNAME}.pdf dist/APPENDIX_A.pdf dist/APPENDIX_B.pdf --outfile dist/${JOBNAME}.pdf --rotateoversize false &>/dev/null
 
 echo "Removing build files..."
 
