4a5049be48e3c0514f82823275b96f5b60e08876
Dmytro Bogatov <dmytro@dbogatov.org>
2017-04-12 18:41:51 -0400

    Add revisions.

---
+++ revisions/build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+INTERACTION=nonstopmode
+OUTDIR=dist
+COMPILATIONS=3
+JOBNAME=revision
+
+echo "Generating document..."
+
+rm -rf $OUTDIR
+mkdir -p $OUTDIR
+
+# LaTeX needs >1 compilations for references
+for (( i=1; i<=$COMPILATIONS; i++ ))
+do
+	xelatex --interaction=${INTERACTION} -output-directory=${OUTDIR} -jobname=${JOBNAME} revision.tex &>/dev/null
+done
+
+echo "Done."
+
+echo "Removing build files..."
+
+rm dist/*.{aux,log,nav,out,snm,toc}
+
+echo "Done."
