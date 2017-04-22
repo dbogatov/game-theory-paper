76211dfcd81a1ecc0d340445fd92ad23506dba41
Dmytro Bogatov <dmytro@dbogatov.org>
2017-04-22 18:40:41 -0400

    Final fixes.

--- paper/build.sh
+++ paper/build.sh
@@ -13,7 +13,7 @@ mkdir -p $OUTDIR
 # LaTeX needs >1 compilations for references
 for (( i=1; i<=$COMPILATIONS; i++ ))
 do
-	xelatex --interaction=${INTERACTION} -output-directory=${OUTDIR} -jobname=${JOBNAME} src/main.tex #&>/dev/null
+	xelatex --interaction=${INTERACTION} -output-directory=${OUTDIR} -jobname=${JOBNAME} src/main.tex &>/dev/null
 done
 
 echo "Done."
@@ -22,7 +22,7 @@ echo "Attaching appendicies..."
 
 enscript -o - src/appendicies/APPENDIX_A.swift | ps2pdf - > dist/APPENDIX_A.pdf
 enscript -o - src/appendicies/APPENDIX_B.txt | ps2pdf - > dist/APPENDIX_B.pdf
-pdfjoin dist/${JOBNAME}.pdf dist/APPENDIX_A.pdf dist/APPENDIX_B.pdf --outfile dist/${JOBNAME}.pdf --rotateoversize false #&>/dev/null
+pdfjoin dist/${JOBNAME}.pdf dist/APPENDIX_A.pdf dist/APPENDIX_B.pdf --outfile dist/${JOBNAME}.pdf --rotateoversize false &>/dev/null
 
 echo "Removing build files..."
 
